943d17299791c31e5b6ffc0175635931288f586b
Donifide <dtadaya34@icloud.com>
2017-02-23 11:56:37 -0700

    Raw data files for your pleasure.
    Here I added .csv data consisting of scans of the same currency, same denomination. Different sensors (First Column: 1-8), Different directions (LD, LU, RD, RU). 
    
    In order to create a training set for analysis (like for the Mahalanobis Classification), you need to note that each Sensor is unique to each Direction. 
    
    [TACHS. csv]: Is simply a header for your own use. I use it just in case I have to open the files using Excel. It is automatically applied as the header, which makes locating/counting the variables (tachs) much easier. 
    
    Creating target-columns
    [C_string_appendor]: is a program that appends a new column of {C}'s to each entity. This helps for machine learning algorithms that require 'target values'. Counterfeit or Check data tagging. 
    [L_string_appendor]: is a program that appends a new column of {L}'s to each entity. This helps for machine learning algorithms that require 'target values'.  Legit data tagging. 
    
    HAVE FUN!

---
+++ C_string_appendor.py
@@ -0,0 +1,41 @@
+1#legit = 0 
+#cft = 1
+#pre = input('Press 1 for CFT or 0 for LEGIT:   ')
+pre = 'C'
+prefix = pre + ','
+suffix = ''
+
+print("You are appending " +pre+ "'s to the beginning of each line.")
+
+#-------------------------------------------------------------
+#*************************************************************
+#insert file name here... make sure it belongs in the same directory
+
+
+file = input('Paste name of file here: ') 
+
+
+#*************************************************************
+#-------------------------------------------------------------
+
+
+#-------------------------------------------------------------
+
+
+with open(file +'.csv', 'r') as src:
+    with open(file + '_appended_' +pre+ '.csv', 'w') as dest:
+       for line in src:
+           dest.write('%s%s%s\n' % (prefix, line.rstrip('\n'), suffix))
+           
+           
+           
+#-------------------------------------------------------------
+           
+#-------------------------------------------------------------   
+
+
+print ("""\nHello Python User! \n\nYou decided to append file: """ + file + """  
+\n...please check to see if the file uploaded just fine.""")
+
+
+#-------------------------------------------------------------        
